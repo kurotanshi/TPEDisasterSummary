4b80ff63f05abb20c03ad123485ed9e1c92bd85d
Kuro Hsu <kurotanshi@gmail.com>
2019-07-22 17:09:21 +0800

    1. 更新資料來源 2. google map 換成 open street map

--- rain/js/script.js
+++ rain/js/script.js
@@ -1,9 +1,11 @@
-(function(){
+(function() {
   var carArr;
   carArr = [];
-  d3.csv("/taipei.csv").row(function(d){
-    if (carArr.length === 0) {
-      return carArr.push(d.CarNo);
-    }
-  }).get(function(error, rows){});
-}).call(this);
+  d3.csv('/taipei.csv')
+    .row(function(d) {
+      if (carArr.length === 0) {
+        return carArr.push(d.CarNo);
+      }
+    })
+    .get(function(error, rows) {});
+}.call(this));
